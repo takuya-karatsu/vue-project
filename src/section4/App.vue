3136e95786de334898c8eee3ed11dd5240bb1fd3
takuya-karatsu <ktakuku14@gmail.com>
2025-01-09 16:27:31 +0900

    section4

---
+++ src/section4/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+const user = ref({
+  name: 'Yoshipi',
+  age: 25,
+  gender: 'man',
+})
+</script>
+<template>
+  <!-- <p v-for="value, key, index in user" :key="value">{{ key }}: {{ value }}({{ index }})</p> -->
+  <p v-for="n of 10" :key="n">{{ n }}</p>
+</template>
