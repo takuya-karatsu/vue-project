6910413c7669456d2b04b6b252ad6f445f3ec685
takuya-karatsu <ktakuku14@gmail.com>
2025-01-10 12:33:03 +0900

    section6

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineEmits(['resetCount'])
+// const emit = defineEmits(['resetCount'])
+// function emitReset() {
+//   emit('resetCount', 100)
+// }
+</script>
+<template>
+  <!-- <button @click="emitReset">Reset</button> -->
+  <button @click="$emit('resetCount', 100)">Reset</button>
+</template>
