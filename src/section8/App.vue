0342837de62e0e5ca7f5dec927ddd6d3804babb3
takuya-karatsu <ktakuku14@gmail.com>
2025-02-17 17:25:02 +0900

    section8

---
+++ src/section8/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, useTemplateRef, onMounted } from 'vue'
+import CountUp from './CountUp.vue'
+const isShow = ref(true)
+const userInput = useTemplateRef('user-input')
+console.log(userInput.value)
+onMounted(() => {
+  console.log(userInput.value)
+})
+</script>
+<template>
+  <button @click="isShow = !isShow">switch</button>
+  <CountUp v-if="isShow" />
+  <p><input ref="user-input" type="text" /></p>
+</template>
---
+++ src/section8/CountUp.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  nextTick,
+} from 'vue'
+const count = ref(0)
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+})
+onMounted(() => {
+  console.log('onMounted')
+})
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+onUpdated(() => {
+  console.log('onUpdated')
+})
+onBeforeUnmount(() => {
+  console.log('onBeforeUnMount')
+})
+onUnmounted(() => {
+  console.log('onUnMounted')
+})
+async function countUp() {
+  count.value++
+  await nextTick()
+  console.log('nextTick')
+}
+</script>
+<template>
+  <p>count: {{ count }}</p>
+  <button @click="countUp">+1</button>
+</template>
